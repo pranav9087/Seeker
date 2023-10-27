ce14be63d15beecf0c86a97160c506f93eef91c8
PeizeLi0106 <peizeli3@illinois.edu>
2023-10-27 16:42:33 -0500

    Fixes on each route

--- backend/app.py
+++ backend/app.py
@@ -48,11 +48,14 @@ def pickInterests():
         interest1 = interests_in_json['interest1']
         interest2 = interests_in_json['interest2']
         interest3 = interests_in_json['interest3']
+        email = interests_in_json['email']
         with sql.connect('./instance/seeker.db') as con:
             cur = con.cursor()
             res = cur.execute("SELECT interest_id FROM interests WHERE interest_name = ? OR interest_name = ? OR interest_name = ?", (interest1, interest2, interest3))
-            [(interest_id1, ), (interest_id2, ), (interest_id3, )] = res.fetchall()
-            res = cur.execute("UPDATE user_interest SET interest_1 = ?, interest_2 = ?, interest_3 = ? WHERE email = ?", (interest_id1, interest_id2, interest_id3, "someemailgetfromloginsession"))
+            interest_id_list = [interest_ids[0] for interest_ids in res.fetchall()]
+            for i in range(len(interest_id_list)):
+                id = i + 1
+                res = cur.execute(f"UPDATE user_interest SET interest_{id} = ? WHERE email = ?", (interest_id_list[i], email))
             con.commit()
     except:
         con.rollback()
@@ -68,26 +71,22 @@ def pickInterests():
 """
 @app.route('/home', methods = ['GET'])
 def home():
-    return_dict  = {}
     status_code = 200
     msg = "Success"
     try:
-        interests_in_json = request.form.get('interests') # 'interest1': "", ....
+        interests_in_json = request.form.get('interests') 
         interest1 = interests_in_json['interest1']
         interest2 = interests_in_json['interest2']
         interest3 = interests_in_json['interest3']
         with sql.connect("./instance/seeker.db") as con:
            cur = con.cursor()
            res = cur.execute("SELECT club_name FROM club INNER JOIN interests ON interest_id WHERE interest_name = ? OR interest_name = ? OR interest_name = ?", (interest1, interest2, interest3))
-           [(clubname1, ), (clubname2, ), (clubname3, )] = res.fetchall()
-        return_dict["clubname1"] = clubname1
-        return_dict["clubname2"] = clubname2
-        return_dict["clubname3"] = clubname3
+           club_list = [clubs[0] for clubs in res.fetchall()]
     except:
         status_code = 500 # db error
     finally:
        con.close()
-       return return_dict, status_code
+       return {"clubs": club_list}, status_code
   
 """
     route for registering new users; responsible for adding user information into the database
@@ -95,19 +94,20 @@ def home():
 @app.route('/register', methods = ['POST'])
 def register():
     status_code = 200
-    msg = "success"
+    return_dict = {}
     try:
         username = request.form.get("username")
         email_address = request.form.get("email_address")
         user_input_password = request.form.get("password")
         password = hash_password(user_input_password)
-
+        return_dict["username"] = username
+        return_dict["email address"] = email_address
         try:
             emailinfo = validate_email(email_address, check_deliverability=True)
             email = emailinfo.normalized
+
         except EmailNotValidError as e:
             status_code = 400 # status code for invalid email
-            msg = "invalid email"
 
         with sql.connect("./instance/seeker.db") as con:
             cur = con.cursor()
@@ -119,11 +119,10 @@ def register():
         if status_code != 400: # if status code is not 400 but it gives an exception, then that means it is an error related to DB mostly on insertion
             con.rollback()
             status_code = 500
-            msg = "Email already used"
     
     finally:
         con.close()
-        return msg, status_code
+        return return_dict, status_code
    
 
 @app.route('/login', methods=['GET', 'POST'])
